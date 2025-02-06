ecedace9532f94d967001cadcc3232f36efa1ceb
Developer@2446 <balu2446madhu@gmail.com>
2025-02-07 05:17:28 +0530

    changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
 
     if (email.value  && password.value) {
       try {
-        const response = await fetch('http://localhost:3000/api/auth/admin-api/admin-user-login/admin-login', {
+        const response = await fetch('/api/auth/admin-api/admin-user-login/admin-login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
