f285e18df19bc915127bcede5945689ed56c9121
Developer@2446 <balu2446madhu@gmail.com>
2025-02-07 05:31:23 +0530

    changed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
 
     if (email.value  && password.value) {
       try {
-        const response = await fetch('/api/auth/admin-api/admin-user-login/admin-login', {
+        const response = await fetch('https://api.paidsms.org/api/auth/admin-api/admin-user-login/admin-login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
